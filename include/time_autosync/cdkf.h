c67bbb891bdecefd69b351766c439fa3e93fdc94
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-05-06 13:16:55 +0200

    absolute timestamps and robust to framedrop

--- include/time_autosync/cdkf.h
+++ include/time_autosync/cdkf.h
@@ -16,15 +16,17 @@ class CDKF {
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
     // inital values
-    double inital_delta_t = 0.1;
+    double inital_delta_t = 0.05;
     double inital_offset = 0.0;
 
     // inital noise values
+    double inital_timestamp_sd = 0.5;
     double inital_delta_t_sd = 0.1;
     double inital_offset_sd = 0.2;
 
     // measurement noise
     double timestamp_sd = 0.02;
+    double angular_velocity_sd = 0.02;
 
     // process noise
     double delta_t_sd = 0.0001;
@@ -32,33 +34,73 @@ class CDKF {
   };
 
   CDKF(const Config& config) {
-    state_.resize(kStateCoreSize, 1);
+    state_.resize(kStateSize, 1);
     accessS(state_, DELTA_T).array() = config.inital_delta_t;
     accessS(state_, OFFSET).array() = config.inital_offset;
 
-    Eigen::MatrixXd inital_sd(kStateCoreSize, 1);
+    Eigen::MatrixXd inital_sd(kStateSize, 1);
+    accessS(inital_sd, STATE_TIMESTAMP).array() = config.inital_timestamp_sd;
     accessS(inital_sd, DELTA_T).array() = config.inital_delta_t_sd;
     accessS(inital_sd, OFFSET).array() = config.inital_offset_sd;
 
-    cov_.resize(kStateCoreSize, kStateCoreSize);
+    cov_.resize(kStateSize, kStateSize);
     cov_.setZero();
     cov_.diagonal() = inital_sd.array() * inital_sd.array();
 
     // set noise sd
-    prediction_noise_sd_.resize(kPredictionNoiseSize, 1);
-    accessS(prediction_noise_sd_, DELTA_T_NOISE).array() = config.delta_t_sd;
-    accessS(prediction_noise_sd_, OFFSET_NOISE).array() = config.offset_sd;
+    prediction_noise_sd_.resize(kStateSize, 1);
+    accessS(prediction_noise_sd_, DELTA_T).array() = config.delta_t_sd;
+    accessS(prediction_noise_sd_, OFFSET).array() = config.offset_sd;
 
-    measurement_noise_sd_.resize(kMeasurementNoiseSize, 1);
-    measurement_noise_sd_.array() = config.timestamp_sd;
+    measurement_noise_sd_.resize(kMeasurementSize, 1);
+    accessM(measurement_noise_sd_, MEASURED_TIMESTAMP).array() =
+        config.timestamp_sd;
+    /*accessM(measurement_noise_sd_, ANGULAR_VELOCITY_NOISE).array() =
+        config.angular_velocity_sd;*/
+
+    zero_timestamp_ = ros::Time::now();
   }
 
-  void predictionUpdate() {
+  // make all stored timestamps relative to this one, called periodically to
+  // prevent loss in precision
+  void rezeroTimestamps() {
+    ros::Time new_zero_timestamp = ros::Time::now();
+    double time_diff = (new_zero_timestamp - zero_timestamp_).toSec();
+    accessS(state_, STATE_TIMESTAMP).array() -= time_diff;
+
+    zero_timestamp_ = new_zero_timestamp;
+  }
+
+  // sync the measured timestamp based on the current filter state
+  void getSyncTimestamp(const ros::Time& received_timestamp,
+                        ros::Time* synced_timestamp) {
+    *synced_timestamp =
+        zero_timestamp_ + ros::Duration(accessS(state_, STATE_TIMESTAMP)[0]);
+
+    double delta_t = accessS(state_, DELTA_T)[0];
+
+    // account for sync being some frames behind
+    int num_frames =
+        std::round((received_timestamp - *synced_timestamp).toSec() / delta_t);
+
+    if ((num_frames > 2) || (num_frames < 0)) {
+      ROS_WARN_STREAM("Timesync is now off by "
+                      << num_frames
+                      << " frames, something must be going horribly wrong");
+    }
+    *synced_timestamp += ros::Duration(num_frames * delta_t);
+
+    // finally account for the offset
+    *synced_timestamp += ros::Duration(accessS(state_, OFFSET)[0]);
+  }
+
+  void predictionUpdate(const ros::Time& received_timestamp) {
     ROS_WARN_STREAM("Initial State: \n" << state_.transpose());
     ROS_WARN_STREAM("Initial Cov: \n" << cov_);
 
     StateSigmaPoints sigma_points(state_, cov_, prediction_noise_sd_,
-                                  CDKF::propergateState);
+                                  CDKF::propergateState,
+                                  received_timestamp - zero_timestamp_);
 
     sigma_points.calcEstimatedMean(&state_);
     sigma_points.calcEstimatedCov(&cov_);
@@ -67,11 +109,14 @@ class CDKF {
     ROS_WARN_STREAM("Predicted Cov: \n" << cov_);
   }
 
-  void measurementUpdate(const double measured_delta_t) {
-
+  void measurementUpdate(
+      const ros::Time& prev_stamp, const ros::Time& current_stamp) {
     // convert tracked points to measurement
     Eigen::VectorXd real_measurement(kMeasurementElementSize);
-    accessM(real_measurement, TIMESTAMP_DELTA).array() = measured_delta_t;
+    accessM(real_measurement, MEASURED_TIMESTAMP).array() =
+        (current_stamp - zero_timestamp_).toSec();
+
+    ROS_WARN_STREAM("measured " << real_measurement.transpose());
 
     // create sigma points
     MeasurementSigmaPoints sigma_points(state_, cov_, measurement_noise_sd_,
@@ -82,6 +127,8 @@ class CDKF {
     sigma_points.calcEstimatedMean(&predicted_measurement);
     Eigen::MatrixXd innovation;
     sigma_points.calcEstimatedCov(&innovation);
+
+    ROS_WARN_STREAM("predicted " << predicted_measurement.transpose());
 
     // calc mah distance
     Eigen::VectorXd diff = real_measurement - predicted_measurement;
@@ -104,22 +151,42 @@ class CDKF {
 
     ROS_WARN_STREAM("Updated State: \n" << state_.transpose());
     ROS_WARN_STREAM("Updated Cov: \n" << cov_);
+
+    // guard against precision issues
+    constexpr double kMaxTime = 10000.0;
+    if (accessS(state_, STATE_TIMESTAMP)[0] > kMaxTime) {
+      rezeroTimestamps();
+    }
   }
 
   static void stateToMeasurementEstimate(
       const Eigen::VectorXd& input_state, const Eigen::VectorXd& noise,
       Eigen::Ref<Eigen::VectorXd> estimated_measurement) {
-    accessM(estimated_measurement, TIMESTAMP_DELTA) =
-        accessS(input_state, DELTA_T) + accessM(noise, TIMESTAMP_NOISE);
+    accessM(estimated_measurement, MEASURED_TIMESTAMP) =
+        accessS(input_state, STATE_TIMESTAMP) + accessM(noise, STATE_TIMESTAMP);
   }
 
-  static void propergateState(const Eigen::VectorXd& noise,
+  static void propergateState(const ros::Duration& zeroed_receive_timestamp,
+                              const Eigen::VectorXd& noise,
                               Eigen::Ref<Eigen::VectorXd> current_state) {
-    accessS(current_state, DELTA_T) += accessS(noise, DELTA_T_NOISE);
-    accessS(current_state, OFFSET) += accessS(noise, OFFSET_NOISE);
+    // work out how many frames to go forward to guard against drops
+    int num_frames = std::round((zeroed_receive_timestamp.toSec() -
+                                 accessS(current_state, STATE_TIMESTAMP)[0]) /
+                                accessS(current_state, DELTA_T)[0]);
+    if (num_frames < 1) {
+      num_frames = 1;
+    }
+
+    accessS(current_state, DELTA_T) += accessS(noise, DELTA_T);
+    accessS(current_state, OFFSET) += accessS(noise, OFFSET);
+
+    accessS(current_state, STATE_TIMESTAMP) +=
+        accessS(current_state, DELTA_T) * num_frames;
   }
 
  private:
+  ros::Time zero_timestamp_;
+
   Eigen::VectorXd state_;
   Eigen::MatrixXd cov_;
 
--- src/time_autosync.cpp
+++ src/time_autosync.cpp
@@ -5,8 +5,13 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
 
-TimeAutosync::TimeAutosync(const ros::NodeHandle &nh,
-                           const ros::NodeHandle &nh_private)
+#include <fstream>
+#include <iostream>
+
+std::ofstream myfile;
+
+TimeAutosync::TimeAutosync(const ros::NodeHandle& nh,
+                           const ros::NodeHandle& nh_private)
     : nh_(nh),
       nh_private_(nh_private),
       it_(nh_private_),
@@ -18,6 +23,18 @@ TimeAutosync::TimeAutosync(const ros::NodeHandle &nh,
   nh_private_.param("max_imu_data_age_s", max_imu_data_age_s_,
                     max_imu_data_age_s_);
 
+  image_sub_ =
+      it_.subscribe("input/image", 10, &TimeAutosync::imageCallback, this);
+
+  imu_sub_ =
+      nh_private_.subscribe("input/imu", 100, &TimeAutosync::imuCallback, this);
+
+  image_pub_ = image_pub_ = it_.advertise("output/image", 10);
+
+  myfile.open("/home/z/Desktop/test.csv");
+}
+
+void TimeAutosync::setupCDKF() {
   CDKF::Config config;
 
   nh_private_.param("inital_delta_t", config.inital_delta_t,
@@ -36,22 +53,11 @@ TimeAutosync::TimeAutosync(const ros::NodeHandle &nh,
   nh_private_.param("offset_sd", config.offset_sd, config.offset_sd);
 
   cdkf_ = std::unique_ptr<CDKF>(new CDKF(config));
-
-  image_sub_ =
-      it_.subscribe("input/image", 10, &TimeAutosync::imageCallback, this);
-
-  image_pub_ = image_pub_ = it_.advertise("output/image", 10);
 }
 
-double TimeAutosync::calcAngleFromImage(const cv::Mat &image) {
-  static cv::Mat prev_image;
-
-  if (prev_image.empty()) {
-    prev_image = image;
-    return 0.0;
-  }
-
-  constexpr int kMaxCorners = 100;
+double TimeAutosync::calcAngleBetweenImages(const cv::Mat& prev_image,
+                                            const cv::Mat& image) {
+  constexpr int kMaxCorners = 1000;
   constexpr double kQualityLevel = 0.01;
   constexpr double kMinDistance = 10;
 
@@ -91,28 +97,30 @@ double TimeAutosync::calcAngleFromImage(const cv::Mat &image) {
 
   // noramilze values
 
-  constexpr double kFocalLength = 460.0;
+  /*constexpr double kFocalLength = 460.0;
   // close enough for most cameras given the low level of accuracy needed
   const cv::Point2f offset(image.cols / 2.0, image.rows / 2.0);
 
   for (size_t i = 0; i < tracked_points.size(); ++i) {
     tracked_prev_points[i] = (tracked_prev_points[i] - offset) / kFocalLength;
     tracked_points[i] = (tracked_points[i] - offset) / kFocalLength;
-  }
+  }*/
 
-  constexpr double kMaxEpipoleDistance = 1.0;
+  constexpr double kMaxEpipoleDistance = 1e-3;
   constexpr double kInlierProbability = 0.99;
 
   std::vector<uint8_t> inliers;
-  cv::Mat cv_E =
+  cv::Mat cv_F =
       cv::findFundamentalMat(tracked_prev_points, tracked_points, cv::FM_LMEDS,
                              kMaxEpipoleDistance, kInlierProbability, inliers);
 
-  prev_image = image;
-
   Eigen::Matrix3d E, W, K;
 
-  cv::cv2eigen(cv_E, E);
+  K << 458.654, 0.0, 367.215, 0.0, 457.296, 248.375, 0.0, 0.0, 1.0;
+
+  cv::cv2eigen(cv_F, E);
+
+  E = K.transpose() * E * K;
 
   Eigen::JacobiSVD<Eigen::Matrix3d> svd(
       E, Eigen::ComputeThinU | Eigen::ComputeThinV);
@@ -129,8 +137,7 @@ double TimeAutosync::calcAngleFromImage(const cv::Mat &image) {
   return angle;
 }
 
-void TimeAutosync::imuCallback(const sensor_msgs::ImuConstPtr &msg) {
-  ROS_ERROR("IMU CALLBACK");
+void TimeAutosync::imuCallback(const sensor_msgs::ImuConstPtr& msg) {
   static sensor_msgs::Imu prev_msg;
   static bool first_msg = true;
 
@@ -138,6 +145,7 @@ void TimeAutosync::imuCallback(const sensor_msgs::ImuConstPtr &msg) {
     first_msg = false;
     imu_rotations_.emplace_back(msg->header.stamp,
                                 Eigen::Quaterniond(1.0, 0.0, 0.0, 0.0));
+    prev_msg = *msg;
     return;
   }
 
@@ -147,54 +155,134 @@ void TimeAutosync::imuCallback(const sensor_msgs::ImuConstPtr &msg) {
         "results.");
   }
 
-  ros::Duration half_delta_t =
-      (msg->header.stamp - prev_msg.header.stamp) / 2.0;
+  // integrate imu reading
+  double half_delta_t =
+      (msg->header.stamp - prev_msg.header.stamp).toSec() / 2.0;
 
   Eigen::Quaterniond delta_angle =
-      AngleAxisf(half_delta_t.toSec() * (msg->angular_velocity.x() +
-                                         prev_msg.angular_velocity.x()),
-                 Vector3f::UnitX()) *
-      AngleAxisf(half_delta_t.toSec() * (msg->angular_velocity.y() +
-                                         prev_msg.angular_velocity.y()),
-                 Vector3f::UnitY()) *
-      AngleAxisf(half_delta_t.toSec() * (msg->angular_velocity.z() +
-                                         prev_msg.angular_velocity.z()),
-                 Vector3f::UnitZ());
+      Eigen::AngleAxisd(half_delta_t * (msg->angular_velocity.x +
+                                        prev_msg.angular_velocity.x),
+                        Eigen::Vector3d::UnitX()) *
+      Eigen::AngleAxisd(half_delta_t * (msg->angular_velocity.y +
+                                        prev_msg.angular_velocity.y),
+                        Eigen::Vector3d::UnitY()) *
+      Eigen::AngleAxisd(half_delta_t * (msg->angular_velocity.z +
+                                        prev_msg.angular_velocity.z),
+                        Eigen::Vector3d::UnitZ());
 
-  imu_msgs_.emplace_back(prev_msg.header.stamp + half_delta_t,
-                         imu_msgs_.back() * delta_angle);
-  imu_msgs_.back().second.normalize();
+  imu_rotations_.emplace_back(
+      prev_msg.header.stamp + ros::Duration(half_delta_t),
+      imu_rotations_.back().second * delta_angle);
+  imu_rotations_.back().second.normalize();
 
-  while ((imu_msgs_.back().first - imu_msgs_.front().first).toSec() >
+  // clear old data
+  while ((imu_rotations_.back().first - imu_rotations_.front().first).toSec() >
          max_imu_data_age_s_) {
-    imu_msgs_.pop_front();
+    imu_rotations_.pop_front();
   }
+
+  prev_msg = *msg;
 }
 
-void TimeAutosync::imageCallback(const sensor_msgs::ImageConstPtr &msg) {
-  cv_bridge::CvImageConstPtr image = cv_bridge::toCvCopy(msg, "mono8");
+Eigen::Quaterniond TimeAutosync::getInterpolatedImuAngle(
+    const AlignedList<std::pair<ros::Time, Eigen::Quaterniond>>& imu_rotations,
+    const ros::Time& stamp) {
+  Eigen::Quaterniond angle;
+
+  AlignedList<std::pair<ros::Time, Eigen::Quaterniond>>::const_iterator prev_it,
+      it;
+  prev_it = imu_rotations.begin();
+
+  // find location of starting stamp
+  for (it = imu_rotations.begin();
+       (std::next(it) != imu_rotations.end()) && (it->first < stamp);
+       prev_it = it++)
+    ;
+
+  // interpolate to get angle
+  if (prev_it->first == it->first) {
+    angle = it->second;
+  } else {
+    const double delta_t = (it->first - prev_it->first).toSec();
+    double w = (it->first - stamp).toSec() / delta_t;
+
+    // don't extrapolate
+    if (w < 0.0) {
+      ROS_WARN("Trying to get Imu data from too far in the past");
+      w = 0.0;
+    } else if (w > 1.0) {
+      ROS_WARN("Trying to get Imu data from too far in the future");
+      w = 1.0;
+    }
+
+    angle = prev_it->second.slerp(w, it->second);
+  }
+
+  return angle;
+}
+
+double TimeAutosync::getImuAngleChange(
+    const AlignedList<std::pair<ros::Time, Eigen::Quaterniond>>& imu_rotations,
+    const ros::Time& start_stamp, const ros::Time& end_stamp) {
+  Eigen::Quaterniond start_angle =
+      getInterpolatedImuAngle(imu_rotations, start_stamp);
+  Eigen::Quaterniond end_angle =
+      getInterpolatedImuAngle(imu_rotations, end_stamp);
+  Eigen::AngleAxisd diff_angle(start_angle.inverse() * end_angle);
+
+  return diff_angle.angle();
+}
+
+void TimeAutosync::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+
+  ros::Time stamp;
+  if (stamp_on_arrival_) {
+    stamp = ros::Time::now();
+  } else {
+    stamp = msg->header.stamp;
+  }
+
+  cv_bridge::CvImagePtr image = cv_bridge::toCvCopy(msg, "mono8");
+  image->header.stamp = stamp;
 
   // put a 1 image delay in the estimation to allow for the possibility that the
   // image is coming in before the imu messages
   static cv_bridge::CvImage prev_image;
-  static bool first_msg = true;
+  static cv_bridge::CvImage prev_prev_image;
+  static size_t seen_atleast_n_images = 0;
 
-  if (first_msg) {
-    first_msg = false;
+  ROS_ERROR_STREAM("IMAGE CALLBACK " << seen_atleast_n_images);
+
+  if (seen_atleast_n_images == 0) {
+    seen_atleast_n_images = 1;
     prev_image = *image;
+    return;
+  } else if (seen_atleast_n_images == 1) {
+    seen_atleast_n_images = 2;
+    prev_prev_image = prev_image;
+    prev_image = *image;
+
+    // leave creating filter as late as possible as if we are playing off a bag
+    // we need to grab its clock
+    setupCDKF();
     return;
   }
 
-  ROS_ERROR_STREAM("angle: " << calcAngleFromImage(prev_image.image));
+  if (imu_rotations_.size() < 2) {
+    return;
+  }
 
+  double image_angle =
+      calcAngleBetweenImages(prev_prev_image.image, prev_image.image);
+  double imu_angle = getImuAngleChange(
+      imu_rotations_, prev_prev_image.header.stamp, prev_image.header.stamp);
+
+  myfile << image_angle << "," << imu_angle << "\n";
+
+  prev_prev_image = prev_image;
   prev_image = *image;
 
-  /*
-  ros::Time stamp;
-
-  static ros::Time prev_stamp;
-  static bool initalized = false;
-
+  /*ros::Time stamp;
   if (stamp_on_arrival_) {
     stamp = ros::Time::now();
   } else {
@@ -211,15 +299,13 @@ void TimeAutosync::imageCallback(const sensor_msgs::ImageConstPtr &msg) {
     ROS_ERROR(
         "The timestamps just went back in time. If your timings are really "
         "this messed up it may be better to set stamp_on_arrival to true");
-  }
-
-  double delta_t = (stamp - prev_stamp).toSec();
-  prev_stamp = stamp;
+  }*/
 
   // actually run filter
   ROS_ERROR("IMU UPDATE");
-  cdkf_->predictionUpdate();
+  cdkf_->predictionUpdate(prev_image.header.stamp);
   ROS_ERROR("MEASUREMENT UPDATE");
-  cdkf_->measurementUpdate(delta_t);
-  ROS_ERROR("DONE");*/
+  cdkf_->measurementUpdate(prev_prev_image.header.stamp,
+                           prev_image.header.stamp);
+  ROS_ERROR("DONE");
 }
